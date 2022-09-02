184b97621881d3b652737c653b60f40946e237ba
Pranshu-Nemani <108977882+Pranshu-Nemani@users.noreply.github.com>
2022-09-02 13:45:32 +0530

    Update home.dart

--- home.dart
+++ home.dart
@@ -9,105 +9,116 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   @override
+  int _index = 0;
 
   Widget build(BuildContext context) {
     return Scaffold(
-       body: Column(
-            children: <Widget>[
-              SafeArea(
-                child: Container(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage('assets/FinalBG.png'),
-                        fit: BoxFit.cover
-                    ),
-                  ),
-                  child: CustomScrollView(
-                    slivers: [
-                      SliverAppBar(
-                        backgroundColor: Colors.black,
-                        title: Text('X-Uberence 2022'),
-                      ),
-                      SliverToBoxAdapter(
-                        child :
-                        SingleChildScrollView(
-                          scrollDirection: Axis.vertical,
-                          child: Column(
-                            children: <Widget>[
-                              Container(
-                                padding: EdgeInsets.all(200.0),
-                                child: Image(
-                                  image: AssetImage('assets/LOGO FINAL.png'),
-                                ),
-                              ),
-                              Container(
-                                child: Countdown(),
-                              ),
-                              Divider(
-                                  height: 60.0,
-                                  color:Colors.transparent
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                children: <Widget>[
-                                  ElevatedButton(
-                                    onPressed: () {},
-                                    child: Text('Sponsors'),
-                                    style: ElevatedButton.styleFrom(
-                                      primary: Colors.black.withOpacity(0.5),
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(30),
-                                      ),
-                                    ),
-                                  ),
-                                  ElevatedButton(
-                                    onPressed: () {},
-                                    child: Text('About us'),
-                                    style: ElevatedButton.styleFrom(
-                                      primary: Colors.black.withOpacity(0.5),
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(30),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Divider(
-                                  height: 60.0,
-                                  color:Colors.transparent
-                              ),
-                            ],
-                          ),
-                          primary: false,
-                        ),
-
-                      ),
-                    ],
+      body: Column(
+          children: <Widget>[
+            SafeArea(
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/FinalBG.png'),
+                      fit: BoxFit.cover
                   ),
                 ),
+                child: CustomScrollView(
+                  slivers: [
+                    SliverAppBar(
+                      backgroundColor: Colors.black,
+                      title: Text('X-Uberence 2022'),
+                    ),
+                    SliverToBoxAdapter(
+                      child:
+                      SingleChildScrollView(
+                        scrollDirection: Axis.vertical,
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.all(200.0),
+                              child: Image(
+                                image: AssetImage('assets/LOGO FINAL.png'),
+                              ),
+                            ),
+                            Container(
+                              child: Countdown(),
+                            ),
+                            Divider(
+                                height: 60.0,
+                                color: Colors.transparent
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  child: Text('Sponsors'),
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Colors.black.withOpacity(0.5),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(30),
+                                    ),
+                                  ),
+                                ),
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  child: Text('About us'),
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Colors.black.withOpacity(0.5),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(30),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Divider(
+                                height: 60.0,
+                                color: Colors.transparent
+                            ),
+                          ],
+                        ),
+                        primary: false,
+                      ),
+
+                    ),
+                  ],
+                ),
               ),
-            ]
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.business),
-              label: 'Business',
+            BottomAppBar(
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                    onPressed: (){
+                    Navigator.pushReplacementNamed(context, '/Home');
+                    },
+                    icon: Icon(Icons.home),
+                  ),
+                  IconButton(
+                    onPressed: (){
+                      Navigator.pushReplacementNamed(context, '/Events');
+                    },
+                    icon: Icon(Icons.games),
+                  ),
+                  IconButton(
+                    onPressed: (){
+                      Navigator.pushReplacementNamed(context, '/Schedule');
+                    },
+                    icon: Icon(Icons.calendar_month),
+                  ),
+                  IconButton(
+                    onPressed: (){
+                      Navigator.pushReplacementNamed(context, '/Registration');
+                    },
+                    icon: Icon(Icons.people),
+                  ),
+                ],
+              ),
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.school),
-              label: 'School',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.abc)
-            ),
-          ],
-          //onTap:
-        ),
+          ]
+      ),
     );
   }
 }
